80a66e16370ecf046b1b58880811c80810457ef2
ManKoin <rohmankoin@gmail.com>
2018-03-01 21:35:25 +0700

    Create ad-network-config.js

---
+++ ad-network-config.js
@@ -0,0 +1,20 @@
+{
+  "placements": [
+    {
+      "anchor": {
+        "selector": "DIV#main",
+        "index": 2,
+        "sub": {
+          "selector": "br",
+          "all": true,
+        },
+      },
+      "pos": 4,
+      "type": 1,
+      "style": {
+        "top_m": 5,
+        "bot_m": 10,
+      },
+    },
+  ]
+}
