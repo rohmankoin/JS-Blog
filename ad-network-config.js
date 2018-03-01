c58ac02e5115bf4228954d516ae0781b37fd75a8
ManKoin <rohmankoin@gmail.com>
2018-03-01 22:12:13 +0700

    Update ad-network-config.js

--- ad-network-config.js
+++ ad-network-config.js
@@ -5,16 +5,16 @@
         "selector": "DIV#post-body-3609529351503073320",
         "index": 2,
         "sub": {
-          "selector": "br",
+          "selector": "DIV.separator",
           "all": true,
-        },
-      },
+        }
+      }
       "pos": 4,
       "type": 1,
       "style": {
         "top_m": 5,
         "bot_m": 10,
-      },
-    },
+      }
+    }
   ]
 }
